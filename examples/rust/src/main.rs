0fd54a8d729d9bf150af5ac828e7ae0d602eb859
Udit <udit@eskaytech.com>
2023-08-17 12:36:44 +0530

    Update rust deps

--- examples/rust/src/main.rs
+++ examples/rust/src/main.rs
@@ -4,7 +4,7 @@ fn main() -> std::io::Result<()> {
     let mut buf = [0; 1024];
     let len = read_stdin(&mut buf)?;
     let str = std::str::from_utf8(&buf[..len as usize]);
-    println!("stdinString: {}", str.unwrap_or_default());
+    println!("stdinString: {}", str.unwrap_or_default().trim());
 
     Ok(())
 }
